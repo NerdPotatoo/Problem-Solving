62cfbdcf6ec705ede4055bcae6a5691e740663af
Yasir Adnan <134677437+NerdPotatoo@users.noreply.github.com>
2024-01-15 23:21:40 +0600

    cf contests

---
+++ AtCoder/AtCoder Beginner Contest 335 (Sponsored by Mynavi)/Beginner Contest 336/A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    
+    int n;
+    cin >> n;
+    cin.ignore();
+    char s2[n+3];
+    s2[0] = 'L';
+    for(int i = 1; i <= n ; i++){
+       s2[i] = 'o';
+    }
+    s2[n+1] = 'n';
+    s2[n+2] = 'g';
+    for(int i =0; i<(n+3);i++){
+        cout<<s2[i];
+    }
+   
+}
